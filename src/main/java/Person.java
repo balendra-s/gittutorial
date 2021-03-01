d9857b446ef2ca0f0f89994377e2a2b28813683a
Balendra Sounthararajan <balendrs@stud.ntnu.no>
2021-03-01 08:01:05 +0100

    Initial commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    String name;
+    String tlfNo;
+    public Person(String name,String tlfNo)
+    {
+        this.name = name;
+        this.tlfNo = tlfNo;
+    }
+    public static void main(String[] args) {
+        System.out.println(name);
+        System.out.println(tlfNo);
+    }
+}
